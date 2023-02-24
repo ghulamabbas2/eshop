476682853320ea8cbae9fbb6da187b083e046361
Ghulam Abbas <ghulamabbas258@gmail.com>
2023-02-25 00:59:36 +0500

    preapred and deployed api to AWS

--- eshop/eshop/settings.py
+++ eshop/eshop/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 # DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['eshop-env.eba-6agvnmdn.us-east-1.elasticbeanstalk.com', 'localhost']
 
 
 # Application definition
@@ -158,7 +158,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
